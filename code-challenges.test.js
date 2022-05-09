1b1e286425c5579ce0701f5c4c9175bd9bc7f5fb
Cj <100457169+cjpriedel@users.noreply.github.com>
2022-05-09 08:06:27 -0700

    completed assessment

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -14,27 +14,49 @@
 
 // a) Create a test with an expect statement using the variable provided.
 
-const people = [
-  { name: "ford prefect", occupation: "a hitchhiker" },
-  { name: "zaphod beeblebrox", occupation: "president of the galaxy" },
-  { name: "arthur dent", occupation: "a radio employee" }
-]
+
 // Expected output: ["Ford Prefect is a hitchhiker.", "Zaphod Beeblebrox is president of the galaxy.", "Arthur Dent is a radio employee."]
 
+describe("capitalizerArr", () => {
+  it("takes in a mixed array and returns an array with a sentence about each person with their name capitalized and their occupation", () => {
+    const people = [
+      { name: "ford prefect", occupation: "a hitchhiker" },
+      { name: "zaphod beeblebrox", occupation: "president of the galaxy" },
+      { name: "arthur dent", occupation: "a radio employee" }
+    ]
+    expect(capitalizerArr(people)).toEqual(["Ford Prefect is a hitchhiker.", "Zaphod Beeblebrox is president of the galaxy.", "Arthur Dent is a radio employee."])
+  })
+})
 
 // b) Create the function that makes the test pass.
 
-const capitalizerArr = (arr)//arr = people (line 1)
-return arr.map((value) => {
-let splitArray = value.name.split(' ')
-let capitalArr = splitArray[0][0].toUpperCase() + splitArray[0].substring(1) + " " + splitArray[1][0].toUpperCase() + splitArray[1].substring(1)
-return `${capitalArr} is a ${value.occupation}`  
+//PSEUDOCODE
+// take an array in a function
+// map over each value to split all of people.names
+// separates new strings in newly mapped array
+// capitalize the first character and concat the remaining characters
+// concat the two names into a variable
+
+const capitalizerArr = (arr) => {
+  return arr.map((value) => {
+  let splitArr = value.name.split(' ')
+  let capArr = splitArr[0][0].toUpperCase() + splitArr[0].substring(1) + " " + splitArr[1][0].toUpperCase() + splitArr[1].substring(1)
+  return `${capArr} is ${value.occupation}.`  
 })
 }
+
 
 // --------------------2) Create a function that takes in a mixed data array and returns an array of only the REMAINDERS of the numbers when divided by 3.
 
 // a) Create a test with an expect statement using the variables provided.
+
+describe("divThreeRemainder",() => {
+  it("divides all numbers in an array by 3 and returns the remainder",() => {
+      expect(divThreeRemainder(hodgepodge1)).toEqual([ 2, 0, -1, 0 ])        
+      expect(divThreeRemainder(hodgepodge2)).toEqual([ 2, 1, -1 ])        
+  })  
+})
+
 
 const hodgepodge1 = [23, "Heyyyy!", 45, -10, 0, "Yo", false]
 // Expected output: [ 2, 0, -1, 0 ]
@@ -43,6 +65,19 @@ const hodgepodge2 = [5, "Hola", 43, -34, "greetings", true]
 
 
 // b) Create the function that makes the test pass.
+
+//PSEUDOCODE
+//create function that takes array 
+// filter array for numbers only
+// map over filtered array and return remainder of value / 3. 
+
+
+
+const divThreeRemainder = (arr) => {
+  let a = arr.filter(v => typeof v === 'number')
+  return a.map(v => v%3)
+}
+
 
 
 // --------------------3) Create a function that takes in an array of numbers and returns the sum of all the numbers cubed.
@@ -54,5 +89,45 @@ const cubeAndSum1 = [2, 3, 4]
 const cubeAndSum2 = [0, 5, 10]
 // Expected output: 1125
 
+describe("cubert",() => {
+  it("cubes and sums all numbers in array",() => {
+    const cubeAndSum1 = [2, 3, 4]
+    const cubeAndSum2 = [0, 5, 10]
+      expect(cubert(cubeAndSum1)).toEqual(99)        
+      expect(cubert(cubeAndSum2)).toEqual(1125)        
+  })  
+})
+
 
 // b) Create the function that makes the test pass.
+
+
+//PSEUDOCODE
+//take an array
+// map the array and multiple each value to the power of 3 
+// loop over the length of the array adding each value to the next value to get the sum
+
+const cubert = (arr) => {
+  let sum = 0
+  let cubed = arr.map(v => v ** 3)
+  for(let i = 0; i < cubed.length; i++) {
+      sum += cubed[i]
+      }
+  return sum
+  }
+
+
+//   PASS  ./code-challenges.test.js
+//   capitalizerArr
+//     √ takes in a mixed array and returns an array with a sentence about each person with their name capitalized and their occupation (2 ms)
+//   divThreeRemainder
+//     √ divides all numbers in an array by 3 and returns the remainder
+//   cubert
+//     √ cubes and sums all numbers in array
+
+// Test Suites: 1 passed, 1 total
+// Tests:       3 passed, 3 total
+// Snapshots:   0 total
+// Time:        0.539 s, estimated 1 s
+// Ran all test suites.
+// Done in 1.50s.
--- interview-questions.md
+++ interview-questions.md
@@ -7,44 +7,63 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. As a developer, I am creating a Rails application with a model called Cohort that has_many students, but OOPS! I forgot to add the foreign key. How can I fix this mistake? What is the name of the foreign key? Would the foreign key be on the Cohort model or the Student model?
 
-  Your answer:
+  Your answer: The foreign key is student_id that links the tables together
 
-  Researched answer:
+  Researched answer: The foreign key is typically the model name followed by "_id"
 
 
 
 2. Which RESTful routes must always be passed params? Why?
 
-  Your answer:
+  Your answer: Any routes that require input or use .find, such as destroy, update or show routes.  
 
-  Researched answer:
+  Researched answer: Show, update and destroy are the routes that always require params so the specific post of data can be edited/deleted. 
 
 
 
 3. Name three rails generator commands. What is created by each?
 
-  Your answer:
+  Your answer: $rails generate resource, $rails generate model, $rails generate migration. The model creates tables, migration pushes new information to the schema, resource creates a model with routes.
 
-  Researched answer:
+  Researched answer: Resource, Model, Scaffold, Controller and Migration are all commands to generate within Rails. Scaffold generates a controller, model and new route, Model creates a table, resource creates models with routes, migration sends new info to the schema, and controller creates a new controller file that can be modified for specific requests.
 
 
 
 4. Consider the Rails routes below. What is the name of the controller method that would be called by each route? What action would each of the controller methods perform?
 
-action: "GET"    location: /students          
+action: "GET"    location: /students    
+students#index  (shows all students)
 
 action: "POST"   location: /students       
+students#create (makes a new student)
 
 action: "GET"    location: /students/new
+students#new (editable form for student)
 
 action: "GET"    location: /students/2  
+students#show, shows student #2
 
-action: "GET"    location: /students/2/edit    
+action: "GET"    location: /students/2/edit  
+students#edit   (generates form to edit student)
 
-action: "PATCH"  location: /students/2      
+action: "PATCH"  location: /students/2 
+students#update (updates student #2)
 
-action: "DELETE" location: /students/2      
+action: "DELETE" location: /students/2
+students#delete  (deletes student #2)
 
 
 
 5. As a developer, you are making an application to manage your to do list. Create 10 user stories that will help you get your application started. Read more about [user stories](https://www.atlassian.com/agile/project-management/user-stories).
+
+
+1) As a user, I can see all the items of my list on the landing page.
+2) As a user, I can add an item to my personal list when clicked.
+3) As a user, I can navigate from the landing page to my list.
+4) As a user, I can return to the landing page from my list.
+5) As a user, I can enter a new task to the main list.
+6) As a user, I can add the new task to my personal list. 
+7) As a user, I can check items on my personal list as completed.
+8) As a user, I can delete my list
+9) As a user, I can save my list.
+10) As a user, I can mark my list as finished and reset it.
